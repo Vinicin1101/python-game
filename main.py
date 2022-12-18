50ad2c7cda096702cf2058487aafaae849923efe
Vinicin1101 <vserafim_o@outlook.com>
2022-12-18 16:32:00 -0300

    janela

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
 import math
 
+# Variaveis Globais
+GAME_TITLE = "não sei"
+FPS = 60
 # screen size
-screen_width = 400
-screen_heigth = 300
+SCREEN_WIDTH = 800
+SCREEN_HEIGTH = 600
+
+
 
 # pygame window
-    window = pygame.display.set_mode((400,300))
+window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
+pygame.display.set_caption(GAME_TITLE)
+pygame.display.set_icon(pygame.image.load('icon.png'))
 
 run = True
 while run:
 
     # close event
     for e in pygame.event.get():
-        if e.type() == pygame.QUIT:
+        if e.type == pygame.QUIT:
             run = False
 
 pygame.quit()
