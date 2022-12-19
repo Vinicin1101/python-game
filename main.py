2ba26197601ab607c0d76f678c9d435b2390df30
Vinicin1101 <vserafim_o@outlook.com>
2022-12-19 13:20:18 -0300

    torto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import terrain
 
 # Variaveis Globais
 GAME_TITLE = "não sei"
@@ -14,17 +15,33 @@ SCREEN_HEIGTH = 600
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
 pygame.display.set_caption(GAME_TITLE)
 pygame.display.set_icon(pygame.image.load('icon.png'))
+clock = pygame.time.Clock()
+
+# Terreno
+terreno = terrain.Terreno(50, 40)
+terreno.gerarMap()
+mapPoints = terreno.getMapCoordinates()
 
 run = True
 while run:
+
+    clock.tick(FPS)
 
     # close event
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             run = False
+        elif e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
+            terreno.gerarMap()
+            mapPoints = terreno.getMapCoordinates()
+            print("Left Click")
 
-            
+    window.fill(pygame.Color("black"))
 
-    clock.tick(FPS)
+    for p in mapPoints:
+        pygame.draw.circle(window, pygame.Color(
+            "white"), (p[0]*10, p[1]*10), 3)
+
+    pygame.display.update()
 
 pygame.quit()
